b872acfc55cc0d54f1f9a7a4d4067a036f5c2fb6
Joshuaesc <escarezjohnjoshuamanalo@gmail.com>
2025-12-28 19:30:13 +0800

    migrate project to monolith repository design pattern for simplicity start from scratch

--- minsu-rms.Api/Program.cs
+++ minsu-rms.Api/Program.cs
@@ -1,106 +1,13 @@
-﻿using RecordManagementSystem.Infrastructure.Persistence.Data;
-using RecordManagementSystem.Application.Features.Account.Interface;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using RecordManagementSystem.Infrastructure.Repository.Features.Account;
-using RecordManagementSystem.Application.Features.Account.Service;
-using RecordManagementSystem.Infrastructure.Persistence.Seeder;
-using RecordManagementSystem.Infrastructure.Services;   
 using RecordManagementSystem.Middleware;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.EntityFrameworkCore.Internal;
-using RecordManagementSystem.Application.Features.OTP.DTO;
-using RecordManagementSystem.Application.Features.OTP.Interfaces;
-using RecordManagementSystem.Application.Features.OTP.Services;
 using System.Security.Claims;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
-//database connection string
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("default")
-));
-
-//login && registered verification for secure credentials 
-builder.Services.AddIdentity<UserIdentity, IdentityRole>(option => 
-    {
-        option.Password.RequireDigit = false;
-        option.Password.RequiredLength = 3;
-        option.Password.RequireLowercase = false;
-        option.Password.RequireUppercase = false;
-        option.Password.RequireNonAlphanumeric = false;
-
-        option.User.RequireUniqueEmail = false;
-        option.SignIn.RequireConfirmedEmail = false;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders()
-    .AddRoles<IdentityRole>();
-
-
-builder.Services.AddScoped<IAddStudentUserData, AddStudentUserAccountRepository>();
-builder.Services.AddScoped<AddStudentUserAccountServices>();
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<AuthServices>();
-
-builder.Services.AddScoped<IJwtToken, JwtService>();
-
-
-//Email Service
-builder.Services.Configure<EmailSettingsDTO>(
-    builder.Configuration.GetSection("EmailSettings")
-);
-builder.Services.AddScoped<IEmailService, EmailService>();
-
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.WithOrigins("https://localhost:7007")
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
-    });
-     
-});
-
-
-#region--Jwt--
-var jwtSettings = builder.Configuration.GetSection("Jwt");
-var key = Encoding.UTF8.GetBytes(jwtSettings["key"]!);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = true;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings["Issuer"],
-            ValidAudience = jwtSettings["Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            RoleClaimType = ClaimTypes.Role,
-
-            ClockSkew = TimeSpan.Zero 
-        };
-
-    });
-#endregion
 
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
@@ -109,7 +16,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//swagger UI redirection
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -129,17 +36,6 @@ app.MapGet("/", context =>
     context.Response.Redirect("/swagger");
     return Task.CompletedTask;
 });
-
-
-//Seeeded account configuration
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserIdentity>>();
-    var configuration = scope.ServiceProvider.GetRequiredService<IConfiguration>();
-
-    await RoleSeeder.Roles(roleManager, userManager,configuration);
-}
 
 
 app.Run();
